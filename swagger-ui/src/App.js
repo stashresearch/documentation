93faa42802de6864d19f5bedac1611d2cfe4632d
steven89 <steven.salaun@gmail.com>
2020-05-19 19:01:37 +0200

    setup

---
+++ swagger-ui/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './App.css'
+import SwaggerUI from 'swagger-ui'
+import 'swagger-ui/dist/swagger-ui.css'
+
+const DOM_ID = 'swagger-ui-mountpoint'
+
+const DisableTryItOutPlugin = function() {
+  return {
+    statePlugins: {
+      spec: {
+        wrapSelectors: {
+          allowTryItOutFor: () => () => false
+        }
+      }
+    }
+  }
+}
+
+class App extends React.Component {
+
+  componentDidMount () {
+    SwaggerUI({
+      dom_id: `#${DOM_ID}`,
+      url: "https://researcharchives.org/documentation/public/swagger.json",
+      plugins: [
+        DisableTryItOutPlugin
+      ]
+    })
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <div id={ DOM_ID }></div>
+      </div>
+    )
+  }
+}
+
+export default App
